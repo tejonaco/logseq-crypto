7eeeec4b00f60defcd767a2ebcd90be9a1507584
tejonaco <tejonaco@gmail.com>
2024-05-03 09:59:08 +0200

    0

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+<h1 align="center">Logseq Crypto</h1>
---
+++ index.ts
@@ -0,0 +1,9 @@
+import '@logseq/libs'
+
+
+async function main (): Promise<void> {
+  //
+}
+
+// bootstrap
+logseq.ready(main).catch(console.error)

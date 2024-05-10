7c06c02734d5d90b29d36cf4e19ad87a901780c4
tejonaco <tejonaco@gmail.com>
2024-05-10 14:20:24 +0200

    small improvement in cryptography

--- src/crypto.ts
+++ src/crypto.ts
@@ -2,12 +2,12 @@ import * as crypto from 'crypto-browserify'
 import { AESContent } from '..'
 
 const ALGORITHM = 'aes-256-ctr'
-const HASH_OBFUSCATOR = '//*// Random noise to prevent dictionary attacks //*//'
+const SALT = '//*// Random noise to hinder dictionary attacks //*//'
 
 export const secureHash = (password: string): string => {
   /* Hash to store page password */
   const hash = crypto.createHash('sha256')
-  hash.update(password + HASH_OBFUSCATOR)
+  hash.update(password + SALT)
   return hash.digest('hex')
 }
 
@@ -15,7 +15,7 @@ export const encrypt = (text: string, password: string): AESContent => {
   const iv = crypto.randomBytes(16)
   // AES password is made from md5 hash
   const md5 = crypto.createHash('md5')
-  md5.update(password)
+  md5.update(SALT + password)
 
   const cipher = crypto.createCipheriv(ALGORITHM, md5.digest('hex'), iv)
 
@@ -29,7 +29,7 @@ export const encrypt = (text: string, password: string): AESContent => {
 
 export const decrypt = (encryptedData: AESContent, password: string): string => {
   const md5 = crypto.createHash('md5')
-  md5.update(password)
+  md5.update(SALT + password)
   const decipher = crypto.createDecipheriv(ALGORITHM, md5.digest('hex'), Buffer.from(encryptedData.iv, 'hex'))
 
   const decrpyted = Buffer.concat([decipher.update(Buffer.from(encryptedData.content, 'hex')), decipher.final()])
